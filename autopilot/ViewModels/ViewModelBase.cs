7748a2e5aab8fa09e775db90b18dcfb8df5da2c8
AleksandrPolishchuk <aleksandrpolishchuk@gmail.com>
2025-06-06 19:07:40 +0700

    Add project files.

---
+++ autopilot/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace autopilot.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ autopilot/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace autopilot.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
